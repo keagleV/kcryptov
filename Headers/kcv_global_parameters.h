1fd5702be6734e04ed5d81408e41ed539b995288
keagleV <kiarash_sedghi@protonmail.com>
2022-01-19 18:06:53 +0330

    Defining the header files

---
+++ Headers/kcv_global_parameters.h
@@ -0,0 +1,12 @@
+#ifndef KCRYPTOV_KCV_GLOBAL_PARAMETERS_H
+#define KCRYPTOV_KCV_GLOBAL_PARAMETERS_H
+
+/* Number of KCV framework developers */
+#define KCV_NUMBER_OF_DEVELOPERS 20
+
+/* Number of developers for each KCV crypto algorithm */
+#define KCV_NUM_CRYPTOALGO_DEVELOPERS 20
+
+
+
+#endif //KCRYPTOV_KCV_GLOBAL_PARAMETERS_H
---
+++ Headers/kcv_types.h
@@ -0,0 +1,26 @@
+#ifndef KCRYPTOV_KCV_TYPES_H
+#define KCRYPTOV_KCV_TYPES_H
+
+
+/*
+ * Major types of the kcryptov framework
+ *
+ * */
+
+
+typedef signed char s8;
+typedef unsigned char u8;
+
+
+typedef signed short s16;
+typedef unsigned short u16;
+
+
+
+typedef signed int s32;
+typedef unsigned int u32;
+
+
+
+
+#endif //KCRYPTOV_KCV_TYPES_H
